9ea5119b3e6e77ca7cfa8149a6a65eb5a20cf0df
rojojun <zoloman316@gmail.com>
2024-01-31 21:57:22 +0900

    코드 패키지 이상하게 된거 옮김

---
+++ src/test/java/com/example/java8_study/FooTest.java
@@ -0,0 +1,12 @@
+package com.example.java8_study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooTest {
+    @Test
+    void foo테스트() {
+        Foo.run();
+    }
+}
