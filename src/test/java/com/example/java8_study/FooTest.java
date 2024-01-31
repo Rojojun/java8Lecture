a489b7d518b8e8425b04ba2821ab962cf19615ed
rojojun <zoloman316@gmail.com>
2024-01-31 22:56:10 +0900

    3강 자바에서 제공하는 함수형 인터페이스

--- src/test/java/com/example/java8_study/FooTest.java
+++ src/test/java/com/example/java8_study/FooTest.java
@@ -2,11 +2,35 @@ package com.example.java8_study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 class FooTest {
+    private Plus10 plus10;
+
+    public FooTest() {
+        this.plus10 = new Plus10();
+    }
+
     @Test
     void foo테스트() {
         Foo.run();
+    }
+
+    @Test
+    void Plus10테스트() {
+        Function<Integer, Integer> plus10 = i -> i +10;
+        Function<Integer, Integer> multiply2 = i -> i * 2;
+
+        System.out.println(plus10.apply(10));
+
+        Function<Integer, Integer>  multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
+        // T * 2 + 10
+
+        System.out.println(plus10.andThen(multiply2).apply(2));
+        // (10 + T) * 2
+
+        UnaryOperator<Integer> plus10Unary = i -> i + 10;
     }
 }
